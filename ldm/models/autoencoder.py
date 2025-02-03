8973c365dfd6363d9274886092df3b70bb0cc5d9
mbsomeday <137542994@qq.com>
2025-02-03 14:51:51 +0000

    update

--- ldm/models/autoencoder.py
+++ ldm/models/autoencoder.py
@@ -311,6 +311,7 @@ class AutoencoderKL(pl.LightningModule):
             self.init_from_ckpt(ckpt_path, ignore_keys=ignore_keys)
 
     def init_from_ckpt(self, path, ignore_keys=list()):
+        print('In init from ckpt')
         sd = torch.load(path, map_location="cpu")["state_dict"]
         keys = list(sd.keys())
         for k in keys:
