aae8de2157f6f92732b4c43379cd6dc4b4f6bacc
mbsomeday <137542994@qq.com>
2025-02-10 10:38:35 +0000

    update

--- reconst_images.py
+++ reconst_images.py
@@ -61,67 +61,67 @@ ds_path = dataset_dict[ds_name]
 print(f'Input dataset: {ds_name} - txt: {txt_name} \nDataset Path:{ds_path}')
 print(f'Batch size: {batch_size}')
 
-# # model
-# ddconfig = {
-#     'double_z': True,
-#     'z_channels': 4,
-#     'resolution': 256,
-#     'in_channels': 3,
-#     'out_ch': 3,
-#     'ch': 128,
-#     'ch_mult': [1, 2, 4, 4],  # num_down = len(ch_mult)-1
-#     'num_res_blocks': 2,
-#     'attn_resolutions': [],
-#     'dropout': 0.0
-# }
-# lossconfig = {
-#     'target': 'ldm.modules.losses.LPIPSWithDiscriminator',
-#     'params' : {
-#         'disc_start': 50001,
-#         'kl_weight': 0.000001,
-#         'disc_weight': 0.5,
-#     }
-# }
-# model = AutoencoderKL(ddconfig=ddconfig,
-#                       lossconfig=lossconfig,
-#                       embed_dim=4,
-#                       ckpt_path=AE_ckpt
-#                       )
-# model.eval()
-# model = model.to(DEVICE)
-# for param in model.parameters():
-#     param.requires_grad = False
-#
-# # dataset
-# cur_data = my_dataset(ds_dir=ds_path, txt_name=txt_name)
-# cur_loader = DataLoader(cur_data, batch_size=batch_size, shuffle=False)
-#
-# tensorDict_save_dir = os.path.join(cwd, ds_name, txt_name[:-4])
-# if not os.path.exists(tensorDict_save_dir):
-#     os.makedirs(tensorDict_save_dir)
-#
-# rec_dict = {}
-# print('Num of images:', len(cur_data))
-# print(fr'Reconstructed images will save to {tensorDict_save_dir}')
-# print('*' * 60)
-#
-# with torch.no_grad():
-#     for idx, image_dict in enumerate(tqdm(cur_loader)):
-#         images = image_dict['image'].to(DEVICE)
-#         dec, posterior = model(images)
-#         image_paths = image_dict['file_path']
-#
-#         for img_idx, img_path in enumerate(image_paths):
-#             image_save_dir_part = img_path.split(ds_path.split(os.sep)[-1])[-1][1:]
-#             rec_dict.update({image_save_dir_part: dec[img_idx]})
-#
-#         if idx != 0 and ((idx+1) % int(2000/batch_size) == 0 or (idx+1) == len(cur_loader)):
-#             tensorDict_save_name = ds_name + '_' + str(txt_name[:-4]) + '_' + str(idx + 1) + '.pt'
-#             tensorDict_save_path = os.path.join(cwd, ds_name, txt_name[:-4], tensorDict_save_name)
-#             torch.save(rec_dict, tensorDict_save_path)
-#             print(f'.pt file save to {tensorDict_save_path}')
-#             # 重置字典
-#             rec_dict = {}
+# model
+ddconfig = {
+    'double_z': True,
+    'z_channels': 4,
+    'resolution': 256,
+    'in_channels': 3,
+    'out_ch': 3,
+    'ch': 128,
+    'ch_mult': [1, 2, 4, 4],  # num_down = len(ch_mult)-1
+    'num_res_blocks': 2,
+    'attn_resolutions': [],
+    'dropout': 0.0
+}
+lossconfig = {
+    'target': 'ldm.modules.losses.LPIPSWithDiscriminator',
+    'params' : {
+        'disc_start': 50001,
+        'kl_weight': 0.000001,
+        'disc_weight': 0.5,
+    }
+}
+model = AutoencoderKL(ddconfig=ddconfig,
+                      lossconfig=lossconfig,
+                      embed_dim=4,
+                      ckpt_path=AE_ckpt
+                      )
+model.eval()
+model = model.to(DEVICE)
+for param in model.parameters():
+    param.requires_grad = False
+
+# dataset
+cur_data = my_dataset(ds_dir=ds_path, txt_name=txt_name)
+cur_loader = DataLoader(cur_data, batch_size=batch_size, shuffle=False)
+
+tensorDict_save_dir = os.path.join(cwd, ds_name, txt_name[:-4])
+if not os.path.exists(tensorDict_save_dir):
+    os.makedirs(tensorDict_save_dir)
+
+rec_dict = {}
+print('Num of images:', len(cur_data))
+print(fr'Reconstructed images will save to {tensorDict_save_dir}')
+print('*' * 60)
+
+with torch.no_grad():
+    for idx, image_dict in enumerate(tqdm(cur_loader)):
+        images = image_dict['image'].to(DEVICE)
+        dec, posterior = model(images)
+        image_paths = image_dict['file_path']
+
+        for img_idx, img_path in enumerate(image_paths):
+            image_save_dir_part = img_path.split(ds_path.split(os.sep)[-1])[-1][1:]
+            rec_dict.update({image_save_dir_part: dec[img_idx]})
+
+        if idx != 0 and ((idx+1) % int(2000/batch_size) == 0 or (idx+1) == len(cur_loader)):
+            tensorDict_save_name = ds_name + '_' + str(txt_name[:-4]) + '_' + str(idx + 1) + '.pt'
+            tensorDict_save_path = os.path.join(cwd, ds_name, txt_name[:-4], tensorDict_save_name)
+            torch.save(rec_dict, tensorDict_save_path)
+            print(f'.pt file save to {tensorDict_save_path}')
+            # 重置字典
+            rec_dict = {}
 
 
 
