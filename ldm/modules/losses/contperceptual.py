f85c886c096da9950d78dc725f4825ef7022da91
mbsomeday <137542994@qq.com>
2025-03-19 16:14:13 +0000

    update

--- ldm/modules/losses/contperceptual.py
+++ ldm/modules/losses/contperceptual.py
@@ -1,8 +1,16 @@
+import numpy as np
 import torch
 import torch.nn as nn
+from torchvision import models
 
 from taming.modules.losses.vqperceptual import *  # TODO: taming dependency yes/no?
 
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+def load_weights(model, weights):
+    ckpt = torch.load(weights, weights_only=False, map_location=DEVICE)
+    model.load_state_dict(ckpt['model_state_dict'])
+    return model
 
 class LPIPSWithDiscriminator(nn.Module):
     def __init__(self, disc_start, logvar_init=0.0, kl_weight=1.0, pixelloss_weight=1.0,
@@ -29,6 +37,15 @@ class LPIPSWithDiscriminator(nn.Module):
         self.discriminator_weight = disc_weight
         self.disc_conditional = disc_conditional
 
+        ds_weights = r'D:\chrom_download\EfficientB0_dsCls-028-0.991572.pth'
+        self.ds_model = models.efficientnet_b0(weights='IMAGENET1K_V1', progress=True)
+        new_classifier = torch.nn.Sequential(
+            torch.nn.Dropout(p=0.2, inplace=True),
+            torch.nn.Linear(in_features=1280, out_features=4)
+        )
+        self.ds_model.classifier = new_classifier
+        load_weights(self.ds_model, ds_weights)
+
     def calculate_adaptive_weight(self, nll_loss, g_loss, last_layer=None):
         if last_layer is not None:
             nll_grads = torch.autograd.grad(nll_loss, last_layer, retain_graph=True)[0]
@@ -41,6 +58,43 @@ class LPIPSWithDiscriminator(nn.Module):
         d_weight = torch.clamp(d_weight, 0.0, 1e4).detach()
         d_weight = d_weight * self.discriminator_weight
         return d_weight
+
+    def calc_cam(self, model, x):
+        '''
+            x是单张image
+        '''
+        logits = model(x)
+        pred = torch.argmax(logits, dim=1)
+
+        model.zero_grad()
+
+        grad_yc = logits[0, pred]
+        print(f'grad_yc: {grad_yc}')
+        grad_yc.backward()
+
+        w = F.adaptive_avg_pool2d(self.backward_features, 1)    # shape: (batch_size, 1280, 1, 1)
+        # print(f'w: {w.shape}')
+        temp_w = w[0].unsqueeze(0)
+        temp_fl = self.feed_forward_features[0].unsqueeze(0)
+        ac = F.conv2d(temp_fl, temp_w)
+        ac = F.relu(ac)
+
+        Ac = F.interpolate(ac, (224, 224))
+
+        heatmap = Ac
+
+        # 获取mask
+        Ac_min = Ac.min()
+        Ac_max = Ac.max()
+        print(f'Attention map diff: {Ac_max - Ac_min}')
+
+        mask = heatmap.detach().clone()
+        mask.requires_grad = False
+        mask[mask<Ac_max] = 0
+        masked_image = x - x * mask
+
+        return heatmap, mask, masked_image
+
 
     def forward(self, inputs, reconstructions, posteriors, optimizer_idx,
                 global_step, last_layer=None, cond=None, split="train",
@@ -59,12 +113,19 @@ class LPIPSWithDiscriminator(nn.Module):
         kl_loss = posteriors.kl()
         kl_loss = torch.sum(kl_loss) / kl_loss.shape[0]
 
+        # 将attention map也用kl_weight作为权重
+        masked_images = np.ones(shape=inputs.shape)
+        for img_idx, image in enumerate(inputs):
+            heatmap, mask, masked_image = self.calc_cam(self.ds_model, image)
+            masked_images[img_idx] = masked_image
+        masked_images = torch.tensor(masked_images)
+
         # now the GAN part
         if optimizer_idx == 0:
             # generator update
             if cond is None:
                 assert not self.disc_conditional
-                logits_fake = self.discriminator(reconstructions.contiguous())
+                logits_fake = self.discriminator(reconstructions.contiguous()) + self.discriminator(masked_images)
             else:
                 assert self.disc_conditional
                 logits_fake = self.discriminator(torch.cat((reconstructions.contiguous(), cond), dim=1))
@@ -82,12 +143,15 @@ class LPIPSWithDiscriminator(nn.Module):
             disc_factor = adopt_weight(self.disc_factor, global_step, threshold=self.discriminator_iter_start)
             loss = weighted_nll_loss + self.kl_weight * kl_loss + d_weight * disc_factor * g_loss
 
+
+
             log = {"{}/total_loss".format(split): loss.clone().detach().mean(), "{}/logvar".format(split): self.logvar.detach(),
                    "{}/kl_loss".format(split): kl_loss.detach().mean(), "{}/nll_loss".format(split): nll_loss.detach().mean(),
                    "{}/rec_loss".format(split): rec_loss.detach().mean(),
                    "{}/d_weight".format(split): d_weight.detach(),
                    "{}/disc_factor".format(split): torch.tensor(disc_factor),
                    "{}/g_loss".format(split): g_loss.detach().mean(),
+
                    }
             return loss, log
 
